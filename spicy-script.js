b122994b7f3a0eb56ce2f84013e260e4ef9d2713
wezenlau <wezenlau@gmail.com>
2024-12-15 10:00:00 +0000

    added socials arrow

--- spicy-script.js
+++ spicy-script.js
@@ -1,28 +1,49 @@
 onscroll = function() {scrollFunction()};
 
-let opened = false;
+let navOpened = false;
+let socialsOpened = false
 
 function scrollFunction() {
-    if (opened == false) {
-        if (document.documentElement.scrollTop < 550) {
-            document.getElementById("side-nav").style.width = "0"
-            document.getElementById("nav-icon").style.right = "-105%";
-        } else {
-            document.getElementById("nav-icon").style.right = "calc(-100% + 45px)";
-        }
+    if (document.documentElement.scrollTop < 550) {
+        document.getElementById("side-nav").style.width = "0"
+        document.getElementById("nav-icon").style.right = "-105%";
+        document.getElementById("icon1").classList.remove("bar-change")
+        navOpened = false
+    } else if (navOpened == false) {
+        document.getElementById("nav-icon").style.right = "calc(-100% + 45px)";
     }
 }
 
-function controlNav(icon) {
-    if (opened == false) {
+function controlNav() {
+    if (navOpened == false) {
         document.getElementById("side-nav").style.width = "225px";
         document.getElementById("nav-icon").style.right = "calc(-100% + 275px)";
-        icon.classList.toggle("bar-change")
-        opened = true;
+        document.getElementById("icon1").classList.add("bar-change")
+        navOpened = true;
     } else {
         document.getElementById("side-nav").style.width = "0";
         document.getElementById("nav-icon").style.right = "calc(-100% + 45px)";
-        icon.classList.toggle("bar-change")
-        opened = false;
+        document.getElementById("icon1").classList.remove("bar-change")
+        navOpened = false;
+    }
+}
+
+function arrowLoader() {
+    document.getElementById("icon2").classList.add("arrow-loader")
+}
+
+function arrowSwitch() {
+    if (socialsOpened == false) {
+        document.getElementById("socials").style.width = "425px"
+        document.getElementById("socials").style.opacity = "1"
+        document.getElementById("icon2").classList.toggle("arrow-switch")
+        document.getElementById("arrow-icon").style.width = "480px"
+        socialsOpened = true
+    } else {
+        document.getElementById("socials").style.width = "0"
+        document.getElementById("socials").style.opacity = "0"
+        document.getElementById("icon2").classList.toggle("arrow-switch")
+        document.getElementById("arrow-icon").style.width = "55px"
+        socialsOpened = false
     }
 }
